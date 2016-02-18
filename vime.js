b4a15f4fb3f923c5081ff1966afa24b9f4493a8c
Carl <carl.hedgren@gmail.com>
2016-02-18 22:12:24 +0100

    Pathing issues with CI

--- vime.js
+++ vime.js
@@ -150,6 +150,6 @@ var VimeBox = React.createClass({
 
 
 ReactDOM.render(
-    <VimeBox url='../../../some.json'/>,
+    <VimeBox url='thanksyourob.azurewebsites.net/some.json'/>,
     document.getElementById('content')
 );
