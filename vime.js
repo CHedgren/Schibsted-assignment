ef88161897b00f2e559c04d3581ce7ad688f1a89
Carl <carl.hedgren@gmail.com>
2016-02-20 16:36:33 +0100

    pagination?

--- vime.js
+++ vime.js
@@ -6,19 +6,20 @@ var Vime = React.createClass({
         return (
             <section className="vime">
                 <h1>{this.props.index}</h1>
-                <Author className="author" user = {this.props.list.user}/>
-
-                <Blurb className="blurb" blurb = {this.props.list}/>
-                <Stats className="stats" stats = {this.props.list.stats} likes = {this.props.list.metadata.connections.likes} comments= {this.props.list.metadata.connections.comments}/>
+                <Author className="author" user={this.props.list.user}/>
+                <Blurb className="blurb" blurb={this.props.list}/>
+                <Stats className="stats" stats={this.props.list.stats}
+                       likes={this.props.list.metadata.connections.likes}
+                       comments={this.props.list.metadata.connections.comments}/>
             </section>
         );
     }
 });
 
 var Author = React.createClass({
-    render: function() {
-        var imglink= this.props.user.pictures.sizes[3].link;
-
+    render: function () {
+        var imglink = this.props.user.pictures ? this.props.user.pictures.sizes[3].link : "";
+        console.log(typeof imglink);
         var name = this.props.user.name;
         var link = this.props.user.link;
         return (
@@ -41,14 +42,14 @@ var Stats = React.createClass({
         return (
             <div className="stats">
                 <h1>
-                P {plays}
-             </h1>
+                    P {plays}
+                </h1>
                 <h2>
-                L {likes}
-             </h2>
+                    L {likes}
+                </h2>
                 <h3>
-                C {comments}
-             </h3>
+                    C {comments}
+                </h3>
             </div>
         );
     }
@@ -56,12 +57,12 @@ var Stats = React.createClass({
 
 
 var Blurb = React.createClass({
-    render: function() {
+    render: function () {
         var desc = this.props.blurb.description;
         var name = this.props.blurb.name;
         return (
             <div className="blurb">
-                <h1><a href={this.props.blurb.link}> {name}</a> </h1>
+                <h1><a href={this.props.blurb.link}> {name}</a></h1>
                 <p>{desc}</p>
 
             </div>
@@ -72,34 +73,37 @@ var Blurb = React.createClass({
 
 var VimeList = React.createClass({
 
-      render: function() {
+    render: function () {
 
-          var content = this.props.vimes.list ? this.props.vimes.list : [];
-          var bounds = this.props.bounds;
-          var allLikes = this.props.allLikes;
-          var substring = this.props.substring;
+        var content = this.props.vimes.list ? this.props.vimes.list : [];
+        var bounds = this.props.bounds;
+        var allLikes = this.props.allLikes;
+        var substring = this.props.substring;
+        console.log(content.length);
+        var vimes = content.filter(function (e, i) {
+            //return true;
+            return i >= bounds[0] && i < bounds[1]
+        }).filter(function (e) {
 
-        var vimes = content.filter(function(e, i){
-
-            return i >= bounds[0] && i < bounds[1] }).filter(function(e) {
             if (allLikes)
-                return e.user.metadata.connections.likes.total > 10
-            return true;
-
-        }).filter(function(e, i){
-
-            if (e.description.indexOf(substring) > -1){
+                return e.user.metadata.connections.likes.total > 10;
+            else
                 return true;
-            }
-            }).map(function(list, index) {
+
+        }).filter(function (e, i) {
+
+            return e.description ? e.description.match(new RegExp(substring, 'i')) : true;
+
+
+        }).map(function (list, index) {
+
 
             return (
-
-
-                <Vime list={list} index={index} />
-
+                <div key={list.user.uri + Math.random(10)}>
+                    <Vime list={list} index={index}/>
+                </div>
             );
-        });
+        }, this);
 
         return (
             <div className="Vimefeed">
@@ -113,63 +117,67 @@ var VimeList = React.createClass({
 
 var Controls = React.createClass({
 
-    moreChange: function(e) {
+    moreChange: function (e) {
 
-            return this.props.updateFilter(e);
-        },
+        return this.props.updateFilter(e);
+    },
 
 
-
-    render: function(){
+    render: function () {
+        console.log(this.props.paging);
         return (
             <fieldset>
-                <select>
-                    <option value="10">10</option>
+                <select className="more" onChange={this.moreChange}>
+                    <option value="9">10</option>
                     <option value="25">25</option>
                     <option value="50">50</option>
 
                 </select>
-                <label><input type="checkbox" className="likes" onClick={this.moreChange}/>Only user better than 10 likes, please</label>
+                <label><input type="checkbox" className="likes" onClick={this.moreChange}/>Only users better than 10
+                    likes, please</label>
                 <input className="filter" type="text" placeholder="filter" onChange={this.moreChange}/>
-                </fieldset>
+                <button className="next" onClick={this.moreChange}>Next</button>
+            </fieldset>
         );
 
 
     }
-
-
 });
 
 
 var VimeBox = React.createClass({
 
     getInitialState: function () {
-        return {data: [],
+        return {
+            data: [],
             i: 0,
-            bounds: [0,10],
+            bounds: [0, 10],
             substring: "",
-            allLikes: true};
+            allLikes: false,
+            paging: true
+        };
     },
-    handleChange: function(e) {
+    handleChange: function (e) {
 
 
-        switch(e.target.className) {
+        switch (e.target.className) {
             case "more":
-                this.setState({bounds: [0, [10,25,50][(this.setState({i: (this.state.i +1)%3}))]]});
-
+                this.setState({bounds: [0, e.target.value]});
+                console.log(e.target.value);
                 break;
             case "likes":
                 this.setState({allLikes: !this.state.allLikes});
-                console.log(this.state.allLikes)
+
                 break;
             case "filter":
-               this.setState({substring: e.target.value});
+                this.setState({substring: e.target.value});
                 console.log(this.state.substring);
                 break;
-            case "pager":
-                this.setState({bounds: [upper, lower]});
+            case "next":
+                this.setState({bounds: [this.state.bounds[0]+this.state.bounds[1], this.state.bounds[1]+this.state.bounds[1]]});
                 break;
         }
+
     },
 
 
@@ -178,7 +186,7 @@ var VimeBox = React.createClass({
         $.ajax({
             url: this.props.url,
             dataType: 'json',
-            crossDomain: true,
+           // crossDomain: true, Turn of Cors and fix mime types on Azure instead.
             cache: true,
             success: function (data) {
                 this.setState({data: data});
@@ -189,23 +197,23 @@ var VimeBox = React.createClass({
 
             }.bind(this)
         });
-
-
     },
 
     render: function () {
+        if (this.state.data.list > this.state.bounds[1]) {
+            this.setState({paging: true});
+        }
         return (
             <div className="vimeBox">
-                <Controls updateFilter={this.handleChange}/>
+                <Controls updateFilter={this.handleChange} paging={this.state.paging}/>
                 <h1>Vimeo-feed</h1>
-                <VimeList vimes={this.state.data} bounds = {this.state.bounds} allLikes = {this.state.allLikes} substring = {this.state.substring}/>
+                <VimeList vimes={this.state.data} bounds={this.state.bounds} allLikes={this.state.allLikes}
+                          substring={this.state.substring} updateFilter={this.handleChange}/>
 
             </div>
         );
     }
 });
-
-
 
 
 ReactDOM.render(
